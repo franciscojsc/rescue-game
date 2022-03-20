ea02b654df115798288d3641a41c71f274e747ef
Francisco Chaves <francisco.jschaves@gmail.com>
2022-03-20 15:45:27 -0300

    feat: add game loop

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,4 +7,15 @@ function start() {
   $('#fundoGame').append("<div id='inimigo1' class='anima2'><div/>");
   $('#fundoGame').append("<div id='inimigo2'><div/>");
   $('#fundoGame').append("<div id='amigo' class='anima3'><div/>");
+
+  let game = {};
+
+  game.timer = setInterval(() => {
+    moveBackground();
+  }, 30);
+}
+
+function moveBackground() {
+  let left = parseInt($('#fundoGame').css('background-position'));
+  $('#fundoGame').css('background-position', left - 1);
 }
