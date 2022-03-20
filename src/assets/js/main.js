2911d2985607de7fb8d5d3742aa366c7424eaa83
Francisco Chaves <francisco.jschaves@gmail.com>
2022-03-20 16:05:00 -0300

    feat: add player movement

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,12 +10,45 @@ function start() {
 
   let game = {};
 
+  const keys = {
+    W: 87,
+    S: 83,
+    D: 68,
+  };
+
+  game.keyPress = [];
+
+  $(document).keydown(function (e) {
+    game.keyPress[e.which] = true;
+  });
+
+  $(document).keyup(function (e) {
+    game.keyPress[e.which] = false;
+  });
+
   game.timer = setInterval(() => {
     moveBackground();
+    movePlayer(game.keyPress, keys);
   }, 30);
 }
 
 function moveBackground() {
   let left = parseInt($('#fundoGame').css('background-position'));
   $('#fundoGame').css('background-position', left - 1);
+}
+
+function movePlayer(keyPress, keys) {
+  let player = $('#jogador');
+  let pos = parseInt(player.css('top'));
+
+  if (keyPress[keys.W]) {
+    player.css('top', pos - 10);
+  }
+
+  if (keyPress[keys.S]) {
+    player.css('top', pos + 10);
+  }
+
+  if (keyPress[keys.D]) {
+  }
 }
